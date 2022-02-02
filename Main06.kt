f9cd1ddea2e12971d02f270a23fb7c4166c89b5b
study-with-HONG <55612285+study-with-HONG@users.noreply.github.com>
2022-02-03 00:04:38 +0900

    Update Main06.kt

--- Main06.kt
+++ Main06.kt
@@ -23,6 +23,11 @@ fun main(args: Array<String>) {
 //    }
 //    localFuc() // golbalVar값이 10으로 바뀜
 //    println(golbalVar)
+    
+    //// 오버로딩 overLoading - 함수 이름이 같지만 매개변수타입이나 갯수가 다름
+    oLoading(1)
+    oLoading2("Hi")
+    oLoading3(11, "Hello")
 }
 
 fun sum(a:Int, b:Int) = a + b
@@ -35,4 +40,15 @@ fun funParam(a:Int, b:Int, c:(Int, Int) -> Int):Int{
 
 fun hello(tt:(String, String) -> String){ // String 2개 받아서 String으로 반환
     println(tt("안녕", "사와디캅"))
+}
+
+//// 오버로딩
+fun oLoading(x:Int){
+    println("oLoading(x:Int) 호출"
+}
+fun oLoading2(y:String){
+    println("oLoading2(y:String) 호출"
+}
+fun oLoading3(x:Int, y:String){
+    println("oLoading3(x:Int, y:String) 호출"
 }
